4e97de79767b9fc4865ea20056074915cc830313
Kelly Truter <kellytruter@gmail.com>
2019-02-26 22:09:42 +0200

    remove distribution output

--- out/library/showErrorMessage.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const vscode = require("vscode");
-function showErrorMessage(errorMessage) {
-    vscode.window.showErrorMessage(`VSCode UI Scripting: ${errorMessage}`);
-}
-exports.showErrorMessage = showErrorMessage;
-//# sourceMappingURL=showErrorMessage.js.map
--- out/library/showInfoMessage.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const vscode = require("vscode");
-function showInfoMessage(message) {
-    vscode.window.showInformationMessage(`VSCode UI Scripting: ${message}`);
-}
-exports.showInfoMessage = showInfoMessage;
-//# sourceMappingURL=showInfoMessage.js.map
